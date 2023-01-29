ad0e3d3029d77a713f1a6802bceabd61ed36115d
adrienvalles <adrien.valles@etu.umontpellier.fr>
2023-01-29 01:39:38 +0100

    données

--- Programmes/fonctionsmaths.py
+++ Programmes/fonctionsmaths.py
@@ -1,63 +1,17 @@
 from math import sqrt
 
-def pourcentage(a,b):
-    '''
-    :param a: partie de b
-    :param b: total
-    :return: Pourcentage de a par rapport à b
-    '''
-    return a/(b/100)
 
 
-
-def moyenne(liste):
-    '''
-    :param liste: liste contenant des valeurs  entières ou réelles
-    :return: La moyenne des valeurs contenus dans la liste
-    '''
-    total = 0
-    for element in liste:
-        total += element
-    return total/len(liste)
+L=[58.79, 61.46, 62.78, 64.82, 65.41, 65.28, 63.93, 62.43, 60.17, 57.59, 55.68, 51.70, 49.91, 
+    48.12, 46.09, 43.29 ,39.94]
 
 
-def ecart_type(liste, moyenne):
-    '''
-    :param liste: Liste de valeurs
-    :param moyenne: Valeur moyenne de la liste de valeurs
-    :return: Retourne l'écart-type
-    '''
-    variance = 0
-    for element in liste:
-        variance += (element - moyenne)**2
-    variance /= len(liste)
-    return sqrt(variance)
+def moyenne1(L):
+	somme = 0
+	for i in L1:
+		somme+=i
+		moy1=sum(L)/len(L)
+	return moy1
+*
 
-def covariance(liste_x, liste_y, moyenne_x, moyenne_y):
-    '''
-    :param liste_x: liste de valeurs
-    :param liste_y: liste de valeurs
-    :param moyenne_x: valeur moyenne de la liste liste_x
-    :param moyenne_y: valeur moyenne de la liste liste_y
-    :return: Retourne la covariance entre liste_x et liste_y
-    '''
-    resultat = 0
-    taille = len(liste_x)
-    for i in range(taille):
-        resultat += (liste_x[i] - moyenne_x) * (liste_y[i] - moyenne_y)
-    resultat /= taille
-    return resultat
 
-def coefficient_correlation(liste_x, liste_y, moyenne_x, moyenne_y):
-    '''
-    :param liste_x: liste de valeurs
-    :param liste_y: liste de valeurs
-    :param moyenne_x: valeur moyenne de la liste liste_x
-    :param moyenne_y: valeur moyenne de la liste liste_y
-    :return: Retourne la covariance entre liste_x et liste_y
-    '''
-    ecart_type_x = ecart_type(liste_x, moyenne_x)
-    ecart_type_y = ecart_type(liste_y, moyenne_y)
-    if ecart_type_x == 0: ecart_type_x = 1/1000000000
-    if ecart_type_y == 0: ecart_type_y = 1/1000000000
-    return covariance(liste_x, liste_y, moyenne_x, moyenne_y) * 1/(ecart_type_x * ecart_type_y)
